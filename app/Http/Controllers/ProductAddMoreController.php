01193f5a46ee977d2f31e1353b584a15d71a8901
Tandiofrendy <tandio78@gmail.com>
2022-08-11 15:51:59 +0700

    add project

---
+++ app/Http/Controllers/ProductAddMoreController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Models\ProductStock;
+
+class ProductAddMoreController extends Controller
+{
+    public function addMore()
+    {
+        return view("addMore");
+    }
+
+    public function addMorePost(Request $request)
+    {
+        $request->validate([
+            'addmore.*.name' => 'required',
+            'addmore.*.qty' => 'required',
+            'addmore.*.price' => 'required',
+        ]);
+    
+        foreach ($request->addmore as $key => $value) {
+            ProductStock::create($value);
+        }
+    
+        return back()->with('success', 'Record Created Successfully.');
+    }
+}
